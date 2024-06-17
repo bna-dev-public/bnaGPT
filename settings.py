f9e07120fc064a30ca1c2fcf2bf35edab9be664f
Gabriel Pinto <149013501+gabrielgithubo@users.noreply.github.com>
2024-06-17 18:41:37 -0300

    update settings with llm and conversations file

--- settings.py
+++ settings.py
@@ -6,3 +6,10 @@ GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 # Variables
 DEFAULT_LLM_NAME = "llama3-70b-8192"
 DEFAULT_LLM_PROVIDER = "groq"
+DEFAULT_LLM = {
+    "llm_provider": DEFAULT_LLM_PROVIDER,
+    "llm_name": DEFAULT_LLM_NAME,
+}
+
+# paths
+CONVERSATIONS_FILE = "./conversations/saved_conversations.json"
